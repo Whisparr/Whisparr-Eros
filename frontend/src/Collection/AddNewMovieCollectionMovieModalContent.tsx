df7ecf1f185ab41a1241e6008f40bf14f9ac7e55
sampulsar <80544409+sampulsar@users.noreply.github.com>
2026-01-25 09:16:39 +1030

    New: Movie Collections (#24)
    Re-adds Collections from Radarr feature set

---
+++ frontend/src/Collection/AddNewMovieCollectionMovieModalContent.tsx
@@ -0,0 +1,235 @@
+import React, { useCallback, useEffect, useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import AppState from 'App/State/AppState';
+import useMovieCollection from 'Collection/useMovieCollection';
+import CheckInput from 'Components/Form/CheckInput';
+import Form from 'Components/Form/Form';
+import FormGroup from 'Components/Form/FormGroup';
+import FormInputGroup from 'Components/Form/FormInputGroup';
+import FormLabel from 'Components/Form/FormLabel';
+import SpinnerButton from 'Components/Link/SpinnerButton';
+import ModalBody from 'Components/Modal/ModalBody';
+import ModalContent from 'Components/Modal/ModalContent';
+import ModalFooter from 'Components/Modal/ModalFooter';
+import ModalHeader from 'Components/Modal/ModalHeader';
+import useIsWindows from 'Helpers/Hooks/useIsWindows';
+import usePrevious from 'Helpers/Hooks/usePrevious';
+import { inputTypes, kinds } from 'Helpers/Props';
+import { Image } from 'Movie/Movie';
+import MoviePoster from 'Movie/MoviePoster';
+import {
+  addMovie,
+  setMovieCollectionValue,
+} from 'Store/Actions/movieCollectionActions';
+import createDimensionsSelector from 'Store/Selectors/createDimensionsSelector';
+import selectSettings from 'Store/Selectors/selectSettings';
+import { InputChanged } from 'typings/inputs';
+import translate from 'Utilities/String/translate';
+import styles from './AddNewMovieCollectionMovieModalContent.css';
+
+export interface AddNewMovieCollectionMovieModalContentProps {
+  foreignId: number;
+  title: string;
+  year: number;
+  overview?: string;
+  images: Image[];
+  collectionId: number;
+  folder: string;
+  onModalClose: () => void;
+}
+
+function AddNewMovieCollectionMovieModalContent({
+  foreignId,
+  title,
+  year,
+  overview,
+  images,
+  collectionId,
+  folder,
+  onModalClose,
+}: AddNewMovieCollectionMovieModalContentProps) {
+  const dispatch = useDispatch();
+
+  const collection = useMovieCollection(collectionId)!;
+
+  const { isSmallScreen } = useSelector(createDimensionsSelector());
+  const isWindows = useIsWindows();
+
+  const { isAdding, addError, pendingChanges } = useSelector(
+    (state: AppState) => state.movieCollections
+  );
+
+  const wasAdding = usePrevious(isAdding);
+
+  const { settings, validationErrors, validationWarnings } = useMemo(() => {
+    const options = {
+      rootFolderPath: collection.rootFolderPath,
+      monitored: collection.monitored,
+      qualityProfileId: collection.qualityProfileId,
+      searchForMovie: collection.searchOnAdd,
+      tags: collection.tags || [],
+    };
+
+    return selectSettings(options, pendingChanges, addError);
+  }, [collection, pendingChanges, addError]);
+
+  const { monitored, qualityProfileId, rootFolderPath, searchForMovie, tags } =
+    settings;
+
+  const handleInputChange = useCallback(
+    ({ name, value }: InputChanged) => {
+      // @ts-expect-error actions aren't typed
+      dispatch(setMovieCollectionValue({ name, value }));
+    },
+    [dispatch]
+  );
+
+  const handleAddMoviePress = useCallback(() => {
+    dispatch(
+      addMovie({
+        foreignId,
+        title,
+        rootFolderPath: rootFolderPath.value,
+        monitored: monitored.value,
+        qualityProfileId: qualityProfileId.value,
+        searchForMovie: searchForMovie.value,
+        tags: tags.value,
+      })
+    );
+  }, [
+    foreignId,
+    title,
+    rootFolderPath,
+    monitored,
+    qualityProfileId,
+    searchForMovie,
+    tags,
+    dispatch,
+  ]);
+
+  useEffect(() => {
+    if (!isAdding && wasAdding && !addError) {
+      onModalClose();
+    }
+  }, [isAdding, wasAdding, addError, onModalClose]);
+
+  return (
+    <ModalContent onModalClose={onModalClose}>
+      <ModalHeader>
+        {title}
+
+        {!title.includes(String(year)) && year ? (
+          <span className={styles.year}>({year})</span>
+        ) : null}
+      </ModalHeader>
+
+      <ModalBody>
+        <div className={styles.container}>
+          {isSmallScreen ? null : (
+            <div className={styles.poster}>
+              <MoviePoster
+                className={styles.poster}
+                images={images}
+                size={250}
+              />
+            </div>
+          )}
+
+          <div className={styles.info}>
+            {overview ? (
+              <div className={styles.overview}>{overview}</div>
+            ) : null}
+
+            <Form
+              validationErrors={validationErrors}
+              validationWarnings={validationWarnings}
+            >
+              <FormGroup>
+                <FormLabel>{translate('RootFolder')}</FormLabel>
+
+                <FormInputGroup
+                  type={inputTypes.ROOT_FOLDER_SELECT}
+                  name="rootFolderPath"
+                  valueOptions={{
+                    movieFolder: folder,
+                    isWindows,
+                  }}
+                  selectedValueOptions={{
+                    movieFolder: folder,
+                    isWindows,
+                  }}
+                  helpText={translate('AddNewMovieRootFolderHelpText', {
+                    folder,
+                  })}
+                  {...rootFolderPath}
+                  onChange={handleInputChange}
+                />
+              </FormGroup>
+
+              <FormGroup>
+                <FormLabel>{translate('Monitor')}</FormLabel>
+
+                <FormInputGroup
+                  type={inputTypes.CHECK}
+                  name="monitored"
+                  helpText={translate('MonitoredMovieHelpText')}
+                  {...monitored}
+                  onChange={handleInputChange}
+                />
+              </FormGroup>
+
+              <FormGroup>
+                <FormLabel>{translate('QualityProfile')}</FormLabel>
+
+                <FormInputGroup
+                  type={inputTypes.QUALITY_PROFILE_SELECT}
+                  name="qualityProfileId"
+                  {...qualityProfileId}
+                  onChange={handleInputChange}
+                />
+              </FormGroup>
+
+              <FormGroup>
+                <FormLabel>{translate('Tags')}</FormLabel>
+
+                <FormInputGroup
+                  type={inputTypes.TAG}
+                  name="tags"
+                  {...tags}
+                  onChange={handleInputChange}
+                />
+              </FormGroup>
+            </Form>
+          </div>
+        </div>
+      </ModalBody>
+
+      <ModalFooter className={styles.modalFooter}>
+        <label className={styles.searchForMissingMovieLabelContainer}>
+          <span className={styles.searchForMissingMovieLabel}>
+            {translate('StartSearchForMissingMovie')}
+          </span>
+
+          <CheckInput
+            containerClassName={styles.searchForMissingMovieContainer}
+            className={styles.searchForMissingMovieInput}
+            name="searchForMovie"
+            {...searchForMovie}
+            onChange={handleInputChange}
+          />
+        </label>
+
+        <SpinnerButton
+          className={styles.addButton}
+          kind={kinds.SUCCESS}
+          isSpinning={isAdding}
+          onPress={handleAddMoviePress}
+        >
+          {translate('AddMovie')}
+        </SpinnerButton>
+      </ModalFooter>
+    </ModalContent>
+  );
+}
+
+export default AddNewMovieCollectionMovieModalContent;
---
+++ src/Whisparr.Api.V3/Collections/CollectionController.cs
@@ -0,0 +1,279 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using NzbDrone.Common.Extensions;
+using NzbDrone.Core.Configuration;
+using NzbDrone.Core.Datastore.Events;
+using NzbDrone.Core.ImportLists.ImportExclusions;
+using NzbDrone.Core.Messaging.Commands;
+using NzbDrone.Core.Messaging.Events;
+using NzbDrone.Core.Movies;
+using NzbDrone.Core.Movies.Collections;
+using NzbDrone.Core.Movies.Commands;
+using NzbDrone.Core.Movies.Events;
+using NzbDrone.Core.Organizer;
+using NzbDrone.SignalR;
+using Whisparr.Http;
+using Whisparr.Http.REST;
+using Whisparr.Http.REST.Attributes;
+
+namespace Whisparr.Api.V3.Collections
+{
+    [V3ApiController]
+    public class CollectionController : RestControllerWithSignalR<CollectionResource, MovieCollection>,
+                                        IHandle<CollectionAddedEvent>,
+                                        IHandle<CollectionEditedEvent>,
+                                        IHandle<CollectionDeletedEvent>
+    {
+        private readonly IMovieCollectionService _collectionService;
+        private readonly IMovieService _movieService;
+        private readonly IMovieMetadataService _movieMetadataService;
+        private readonly IImportListExclusionService _importListExclusionService;
+        private readonly IConfigService _configService;
+        private readonly IBuildFileNames _fileNameBuilder;
+        private readonly INamingConfigService _namingService;
+        private readonly IManageCommandQueue _commandQueueManager;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CollectionController"/> class.
+        /// </summary>
+        /// <param name="signalRBroadcaster">SignalR broadcaster used to push resource change notifications.</param>
+        /// <param name="collectionService">Service for movie collections.</param>
+        /// <param name="movieService">Service for movies.</param>
+        /// <param name="movieMetadataService">Service to retrieve movie metadata (external sources).</param>
+        /// <param name="importListExclusionService">Service used to check import list exclusions.</param>
+        /// <param name="configService">Application configuration service.</param>
+        /// <param name="fileNameBuilder">Builder for movie folder/file naming.</param>
+        /// <param name="namingService">Naming configuration service.</param>
+        /// <param name="commandQueueManager">Command queue manager used to enqueue background commands.</param>
+        public CollectionController(IBroadcastSignalRMessage signalRBroadcaster,
+                                    IMovieCollectionService collectionService,
+                                    IMovieService movieService,
+                                    IMovieMetadataService movieMetadataService,
+                                    IImportListExclusionService importListExclusionService,
+                                    IConfigService configService,
+                                    IBuildFileNames fileNameBuilder,
+                                    INamingConfigService namingService,
+                                    IManageCommandQueue commandQueueManager)
+            : base(signalRBroadcaster)
+        {
+            _collectionService = collectionService;
+            _movieService = movieService;
+            _movieMetadataService = movieMetadataService;
+            _importListExclusionService = importListExclusionService;
+            _configService = configService;
+            _fileNameBuilder = fileNameBuilder;
+            _namingService = namingService;
+            _commandQueueManager = commandQueueManager;
+        }
+
+        protected override CollectionResource GetResourceById(int id)
+        {
+            return MapToResource(_collectionService.GetCollection(id));
+        }
+
+        /// <summary>
+        /// Retrieves collections. If <paramref name="tmdbId"/> is provided returns the single matching collection;
+        /// otherwise returns all collections.
+        /// </summary>
+        /// <param name="tmdbId">Optional TMDB collection id to filter by.</param>
+        /// <returns>List of <see cref="CollectionResource"/> matching the request.</returns>
+        [HttpGet]
+        [Produces("application/json")]
+        public List<CollectionResource> GetCollections(int? tmdbId)
+        {
+            var collectionResources = new List<CollectionResource>();
+
+            if (tmdbId.HasValue)
+            {
+                var collection = _collectionService.FindByTmdbId(tmdbId.Value);
+
+                if (collection != null)
+                {
+                    collectionResources.AddIfNotNull(MapToResource(collection));
+                }
+            }
+            else
+            {
+                collectionResources = MapToResource(_collectionService.GetAllCollections()).ToList();
+            }
+
+            return collectionResources;
+        }
+
+        /// <summary>
+        /// Updates a single collection.
+        /// </summary>
+        /// <param name="collectionResource">The collection resource containing updated values. The resource's Id is used to locate the existing collection.</param>
+        /// <returns>Accepted result with the updated collection Id.</returns>
+        [RestPutById]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        public ActionResult<CollectionResource> UpdateCollection([FromBody] CollectionResource collectionResource)
+        {
+            var collection = _collectionService.GetCollection(collectionResource.Id);
+
+            var model = collectionResource.ToModel(collection);
+
+            var updatedMovie = _collectionService.UpdateCollection(model);
+
+            return Accepted(updatedMovie.Id);
+        }
+
+        /// <summary>
+        /// Bulk update for multiple collections. Only properties present in the request are applied.
+        /// </summary>
+        /// <param name="resource">The update resource containing collection ids and the properties to change.</param>
+        /// <returns>Accepted result containing the updated collection resources.</returns>
+        [HttpPut]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        public ActionResult UpdateCollections([FromBody] CollectionUpdateResource resource)
+        {
+            var collectionsToUpdate = _collectionService.GetCollections(resource.CollectionIds).ToList();
+
+            foreach (var collection in collectionsToUpdate)
+            {
+                if (resource.Monitored.HasValue)
+                {
+                    collection.Monitored = resource.Monitored.Value;
+                }
+
+                if (resource.QualityProfileId.HasValue)
+                {
+                    collection.QualityProfileId = resource.QualityProfileId.Value;
+                }
+
+                if (resource.RootFolderPath.IsNotNullOrWhiteSpace())
+                {
+                    collection.RootFolderPath = resource.RootFolderPath;
+                }
+
+                if (resource.SearchOnAdd.HasValue)
+                {
+                    collection.SearchOnAdd = resource.SearchOnAdd.Value;
+                }
+
+                if (resource.MonitorMovies.HasValue)
+                {
+                    var movies = _movieService.GetMoviesByCollectionTmdbId(collection.TmdbId);
+
+                    movies.ForEach(c => c.Monitored = resource.MonitorMovies.Value);
+
+                    _movieService.UpdateMovie(movies, true);
+                }
+            }
+
+            var updated = _collectionService.UpdateCollections(collectionsToUpdate).ToResource();
+
+            _commandQueueManager.Push(new RefreshCollectionsCommand());
+
+            return Accepted(updated);
+        }
+
+        private IEnumerable<CollectionResource> MapToResource(List<MovieCollection> collections)
+        {
+            // Avoid calling for naming spec on every movie in filenamebuilder
+            var namingConfig = _namingService.GetConfig();
+
+            var existingMoviesTmdbIds = _movieService.AllMovieWithCollectionsTmdbIds();
+            var listExclusions = _importListExclusionService.GetAllByType(ImportExclusionType.Movie);
+
+            var allCollectionMovies = _movieMetadataService.GetMoviesWithCollections()
+                .GroupBy(x => x.CollectionTmdbId)
+                .ToDictionary(x => x.Key, x => (IEnumerable<MovieMetadata>)x);
+
+            foreach (var collection in collections)
+            {
+                var resource = collection.ToResource();
+
+                allCollectionMovies.TryGetValue(collection.TmdbId, out var collectionMovies);
+
+                if (collectionMovies != null)
+                {
+                    foreach (var movie in collectionMovies)
+                    {
+                        var movieResource = movie.ToResource();
+                        movieResource.Folder = _fileNameBuilder.GetMovieFolder(new Movie { MovieMetadata = movie }, namingConfig);
+
+                        var isExisting = existingMoviesTmdbIds.Contains(movie.TmdbId);
+                        movieResource.IsExisting = isExisting;
+
+                        var isExcluded = listExclusions.Any(e => e.ForeignId == movie.TmdbId.ToString());
+                        movieResource.IsExcluded = isExcluded;
+
+                        if (!isExisting && !isExcluded)
+                        {
+                            resource.MissingMovies++;
+                        }
+
+                        resource.Movies.Add(movieResource);
+                    }
+                }
+
+                yield return resource;
+            }
+        }
+
+        private CollectionResource MapToResource(MovieCollection collection)
+        {
+            var resource = collection.ToResource();
+
+            var namingConfig = _namingService.GetConfig();
+
+            var existingMoviesTmdbIds = _movieService.AllMovieWithCollectionsTmdbIds();
+            var listExclusions = _importListExclusionService.GetAllByType(ImportExclusionType.Movie);
+
+            foreach (var movie in _movieMetadataService.GetMoviesByCollectionTmdbId(collection.TmdbId))
+            {
+                var movieResource = movie.ToResource();
+                movieResource.Folder = _fileNameBuilder.GetMovieFolder(new Movie { MovieMetadata = movie }, namingConfig);
+
+                var isExisting = existingMoviesTmdbIds.Contains(movie.TmdbId);
+                movieResource.IsExisting = isExisting;
+
+                var isExcluded = listExclusions.Any(e => e.ForeignId == movie.TmdbId.ToString());
+                movieResource.IsExcluded = isExcluded;
+
+                if (!isExisting && !isExcluded)
+                {
+                    resource.MissingMovies++;
+                }
+
+                resource.Movies.Add(movieResource);
+            }
+
+            return resource;
+        }
+
+        /// <summary>
+        /// Handles a collection added event and broadcasts the created resource.
+        /// </summary>
+        /// <param name="message">Event message containing the added collection.</param>
+        [NonAction]
+        public void Handle(CollectionAddedEvent message)
+        {
+            BroadcastResourceChange(ModelAction.Created, MapToResource(message.Collection));
+        }
+
+        /// <summary>
+        /// Handles a collection edited event and broadcasts the updated resource.
+        /// </summary>
+        /// <param name="message">Event message containing the edited collection.</param>
+        [NonAction]
+        public void Handle(CollectionEditedEvent message)
+        {
+            BroadcastResourceChange(ModelAction.Updated, MapToResource(message.Collection));
+        }
+
+        /// <summary>
+        /// Handles a collection deleted event and broadcasts the deleted collection id.
+        /// </summary>
+        /// <param name="message">Event message containing the deleted collection.</param>
+        [NonAction]
+        public void Handle(CollectionDeletedEvent message)
+        {
+            BroadcastResourceChange(ModelAction.Deleted, message.Collection.Id);
+        }
+    }
+}
