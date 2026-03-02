3c5f3829f6eabbdec77469a3741c4a072b515a68
plz12345 <132735020+plz12345@users.noreply.github.com>
2026-03-01 20:22:16 -0500

    Fix: Path filter for scenes and movies (#149)

--- src/Whisparr.Api.V3/Movies/Helpers/Paging.cs
+++ src/Whisparr.Api.V3/Movies/Helpers/Paging.cs
@@ -31,51 +31,50 @@ namespace Whisparr.Api.V3.Movies.Helpers
                 {
                     case "added":
                         DateFilter.Apply(pageSpec, jsonElement, op, p => p.Added);
-
                         break;
+
                     case "itemtype":
                         EnumFilterLazy.Apply(pageSpec, jsonElement, op, p => p.MovieMetadata.Value.ItemType);
 
                         break;
                     case "monitored":
                         BooleanFilter.Apply<Movie>(pageSpec, jsonElement, op, p => p.Monitored);
-
                         break;
+
                     case "path":
                         StringFilter.Apply(pageSpec, jsonElement, op, p => p.Path);
-
                         break;
+
                     case "qualityprofileid":
                         NumericFilterInt.Apply(pageSpec, jsonElement, op, p => p.QualityProfileId);
-
                         break;
+
                     case "releasedate":
                         DateFilter.Apply(pageSpec, jsonElement, op, p => p.MovieMetadata.Value.ReleaseDateUtc);
-
                         break;
+
                     case "status":
                         EnumFilterLazy.Apply(pageSpec, jsonElement, op, p => p.MovieMetadata.Value.Status);
-
                         break;
+
                     case "runtime":
                         NumericFilterInt.Apply(pageSpec, jsonElement, op, p => p.MovieMetadata.Value.Runtime);
                         break;
+
                     case "sizeondisk": // Special case due to optional inner join on MovieFile and wanting to support filtering for movies without files (size 0 or null)
                         SizeOnDiskFilter.Apply(pageSpec, jsonElement, op);
                         break;
 
                     case "tags": // In-memory filtering for tags, with special handling for contains and notcontains operations
                         TagsFilter.Apply(pageSpec, jsonElement, op);
-
                         break;
 
                     case "title":
                         StringFilter.Apply(pageSpec, jsonElement, op, p => p.MovieMetadata.Value.Title);
-
                         break;
+
                     case "year":
                         NumericFilterInt.Apply(pageSpec, jsonElement, op, p => p.Year);
-
                         break;
                 }
             }
