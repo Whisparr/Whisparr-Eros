45c661cdabbf4a9131b159909d9a2cd656232095
plz12345 <132735020+plz12345@users.noreply.github.com>
2026-03-13 19:27:02 -0400

    Chore: Upgrade eslint, prettier, stylelint

--- frontend/src/Store/Middleware/middlewares.js
+++ frontend/src/Store/Middleware/middlewares.js
@@ -4,7 +4,7 @@ import thunk from 'redux-thunk';
 import createPersistState from './createPersistState';
 import createSentryMiddleware from './createSentryMiddleware';
 
-export default function(history) {
+export default function (history) {
   const middlewares = [];
   const sentryMiddleware = createSentryMiddleware();
 
@@ -15,8 +15,9 @@ export default function(history) {
   middlewares.push(routerMiddleware(history));
   middlewares.push(thunk);
 
-  // eslint-disable-next-line no-underscore-dangle
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const composeEnhancers =
+    // eslint-disable-next-line no-underscore-dangle
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
   return composeEnhancers(
     applyMiddleware(...middlewares),
