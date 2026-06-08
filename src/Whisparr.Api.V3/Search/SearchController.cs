5d7873e703f04ba04fced97be3f7a0ccc8af4e2f
Mothball7205 <158273459+Mothball7205@users.noreply.github.com>
2026-06-09 00:59:03 +0200

    fix: annotate search lookup results with existing database status

--- src/Whisparr.Api.V3/Search/SearchController.cs
+++ src/Whisparr.Api.V3/Search/SearchController.cs
@@ -27,6 +27,8 @@ namespace Whisparr.Api.V3.Search
         private readonly IConfigService _configService;
         private readonly IImportListExclusionService _exclusionService;
         private readonly IMovieService _movieService;
+        private readonly IStudioService _studioService;
+        private readonly IPerformerService _performerService;
 
         public SearchController(ISearchForNewMovie searchProxy,
                                 IBuildFileNames fileNameBuilder,
@@ -34,7 +36,9 @@ namespace Whisparr.Api.V3.Search
                                 IMapCoversToLocal coverMapper,
                                 IConfigService configService,
                                 IImportListExclusionService exclusionService,
-                                IMovieService movieService)
+                                IMovieService movieService,
+                                IStudioService studioService,
+                                IPerformerService performerService)
         {
             _searchProxy = searchProxy;
             _fileNameBuilder = fileNameBuilder;
@@ -43,6 +47,8 @@ namespace Whisparr.Api.V3.Search
             _configService = configService;
             _exclusionService = exclusionService;
             _movieService = movieService;
+            _studioService = studioService;
+            _performerService = performerService;
         }
 
         [HttpGet("scene")]
@@ -67,19 +73,49 @@ namespace Whisparr.Api.V3.Search
         public object SearchStudio([FromQuery] string term)
         {
             var searchResults = _searchProxy.SearchForNewStudio(term);
-            return MapToResource(searchResults).ToList();
+            var searchResources = MapToResource(searchResults).ToList();
+            MapToExistingStudios(searchResources);
+            return searchResources;
         }
 
         [HttpGet("performer")]
         public object SearchPerformer([FromQuery] string term)
         {
             var searchResults = _searchProxy.SearchForNewPerformer(term);
-            return MapToResource(searchResults).ToList();
+            var searchResources = MapToResource(searchResults).ToList();
+            MapToExistingPerformers(searchResources);
+            return searchResources;
         }
 
         private void MapToExistingMovies(List<SearchResource> searchResources)
         {
             var matches = _movieService.FindByForeignIds(searchResources.Select(m => m.ForeignId).ToList());
+            foreach (var s in searchResources)
+            {
+                var match = matches.Where(m => m.ForeignId == s.ForeignId);
+                if (match.Any())
+                {
+                    s.isExisting = true;
+                }
+            }
+        }
+
+        private void MapToExistingStudios(List<SearchResource> searchResources)
+        {
+            var matches = _studioService.FindByForeignIds(searchResources.Select(s => s.ForeignId).ToList());
+            foreach (var s in searchResources)
+            {
+                var match = matches.Where(m => m.ForeignId == s.ForeignId);
+                if (match.Any())
+                {
+                    s.isExisting = true;
+                }
+            }
+        }
+
+        private void MapToExistingPerformers(List<SearchResource> searchResources)
+        {
+            var matches = _performerService.FindByForeignIds(searchResources.Select(p => p.ForeignId).ToList());
             foreach (var s in searchResources)
             {
                 var match = matches.Where(m => m.ForeignId == s.ForeignId);
