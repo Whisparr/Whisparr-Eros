519a68a74d1b3ccc18fb36e14f3c2eca25c1cdb2
plz12345 <132735020+plz12345@users.noreply.github.com>
2026-08-28 07:55:16 -0400

    Retire Sentry from the frontend (#547)

--- frontend/src/Store/Middleware/middlewares.js
+++ frontend/src/Store/Middleware/middlewares.js
@@ -1,17 +1,9 @@
 import { applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import createPersistState from './createPersistState';
-import createSentryMiddleware from './createSentryMiddleware';
 
 export default function () {
-  const middlewares = [];
-  const sentryMiddleware = createSentryMiddleware();
-
-  if (sentryMiddleware) {
-    middlewares.push(sentryMiddleware);
-  }
-
-  middlewares.push(thunk);
+  const middlewares = [thunk];
 
   const composeEnhancers =
     // eslint-disable-next-line no-underscore-dangle
