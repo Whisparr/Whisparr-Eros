7e9d3d85436aa918a85890b2b16b28f09a14b823
plz12345 <132735020+plz12345@users.noreply.github.com>
2026-08-28 13:25:08 -0400

    Delete the dead frontend JavaScript (#551)

--- frontend/src/Scene/sceneEntities.js
+++
@@ -1,9 +0,0 @@
-export const CALENDAR = 'calendar';
-export const SCENES = 'scenes';
-export const INTERACTIVE_IMPORT = 'interactiveImport.scenes';
-
-export default {
-  CALENDAR,
-  SCENES,
-  INTERACTIVE_IMPORT,
-};
