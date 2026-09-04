0dc1494b7c14bb8ab265fef51863479e31b3417d
plz12345 <132735020+plz12345@users.noreply.github.com>
2026-09-04 19:36:59 -0400

    New: Import list option to tag existing scenes, performers and studios (#645)

--- src/NzbDrone.Core.Test/ImportListTests/ImportListSyncServiceFixture.cs
+++ src/NzbDrone.Core.Test/ImportListTests/ImportListSyncServiceFixture.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using FizzWare.NBuilder;
+using FluentAssertions;
 using Moq;
 using NUnit.Framework;
 using NzbDrone.Core.Configuration;
@@ -8,6 +9,8 @@ using NzbDrone.Core.ImportLists;
 using NzbDrone.Core.ImportLists.ImportExclusions;
 using NzbDrone.Core.ImportLists.ImportListMovies;
 using NzbDrone.Core.Movies;
+using NzbDrone.Core.Movies.Performers;
+using NzbDrone.Core.Movies.Studios;
 using NzbDrone.Core.Test.Framework;
 
 namespace NzbDrone.Core.Test.ImportList
@@ -110,15 +113,43 @@ namespace NzbDrone.Core.Test.ImportList
                         .Returns(cleanLevel);
             }
 
-            private void GivenList(int id, bool enabledAuto)
+            private void GivenList(int id, bool enabledAuto, HashSet<int> tags = null, bool tagExisting = false)
             {
-                  var importListDefinition = new ImportListDefinition { Id = id, EnableAuto = enabledAuto };
+                  var importListDefinition = new ImportListDefinition
+                  {
+                        Id = id,
+                        EnableAuto = enabledAuto,
+                        TagExisting = tagExisting,
+                        Tags = tags ?? new HashSet<int>()
+                  };
 
                   Mocker.GetMock<IImportListFactory>()
                         .Setup(v => v.Get(id))
                         .Returns(importListDefinition);
 
                   CreateListResult(id, enabledAuto);
+            }
+
+            private void GivenExistingMovie(string foreignId, HashSet<int> tags, string studioForeignId = null, params string[] performerForeignIds)
+            {
+                  var movie = new Movie
+                  {
+                        Id = 1,
+                        ForeignId = foreignId,
+                        Title = foreignId,
+                        Tags = tags
+                  };
+
+                  movie.MovieMetadata.Value.StudioForeignId = studioForeignId;
+                  movie.MovieMetadata.Value.PerformerForeignIds = performerForeignIds.ToList();
+
+                  Mocker.GetMock<IMovieService>()
+                        .Setup(v => v.AllMovieForeignIds())
+                        .Returns(new List<string> { foreignId });
+
+                  Mocker.GetMock<IMovieService>()
+                        .Setup(v => v.FindByForeignIds(It.Is<List<string>>(ids => ids.Contains(foreignId))))
+                        .Returns(new List<Movie> { movie });
             }
 
             private Mock<IImportList> CreateListResult(int id, bool enabledAuto)
@@ -433,6 +464,181 @@ namespace NzbDrone.Core.Test.ImportList
 
                   Mocker.GetMock<IAddMovieService>()
                         .Verify(v => v.AddMovies(It.Is<List<Movie>>(s => s.Count == 7 && s.All(m => m.ForeignId != _existingMovies[0].ForeignId)), true), Times.Once());
+            }
+
+            [Test]
+            public void should_not_tag_existing_movie_when_tag_existing_is_disabled()
+            {
+                  _importListFetch.Movies.ForEach(m => m.ListId = 1);
+                  _importListFetch.Movies[0].ForeignId = "ForeignId6";
+
+                  GivenList(1, true, new HashSet<int> { 3 });
+                  GivenCleanLevel("disabled");
+                  GivenExistingMovie("ForeignId6", new HashSet<int>());
+
+                  Subject.Execute(_commandAll);
+
+                  Mocker.GetMock<IMovieService>()
+                        .Verify(v => v.FindByForeignIds(It.IsAny<List<string>>()), Times.Never());
+
+                  Mocker.GetMock<IMovieService>()
+                        .Verify(v => v.UpdateMovie(It.IsAny<List<Movie>>(), It.IsAny<bool>()), Times.Never());
+            }
+
+            [Test]
+            public void should_not_tag_existing_movie_when_list_has_no_tags()
+            {
+                  _importListFetch.Movies.ForEach(m => m.ListId = 1);
+                  _importListFetch.Movies[0].ForeignId = "ForeignId6";
+
+                  GivenList(1, true, new HashSet<int>(), true);
+                  GivenCleanLevel("disabled");
+                  GivenExistingMovie("ForeignId6", new HashSet<int>());
+
+                  Subject.Execute(_commandAll);
+
+                  Mocker.GetMock<IMovieService>()
+                        .Verify(v => v.FindByForeignIds(It.IsAny<List<string>>()), Times.Never());
+            }
+
+            [Test]
+            public void should_tag_existing_movie_when_tag_existing_is_enabled()
+            {
+                  _importListFetch.Movies.ForEach(m => m.ListId = 1);
+                  _importListFetch.Movies[0].ForeignId = "ForeignId6";
+
+                  GivenList(1, true, new HashSet<int> { 3 }, true);
+                  GivenCleanLevel("disabled");
+                  GivenExistingMovie("ForeignId6", new HashSet<int> { 1 });
+
+                  Subject.Execute(_commandAll);
+
+                  Mocker.GetMock<IMovieService>()
+                        .Verify(v => v.UpdateMovie(It.Is<List<Movie>>(m => m.Count == 1 && m[0].Tags.SetEquals(new[] { 1, 3 })), true), Times.Once());
+            }
+
+            [Test]
+            public void should_not_update_existing_movie_that_already_has_the_tags()
+            {
+                  _importListFetch.Movies.ForEach(m => m.ListId = 1);
+                  _importListFetch.Movies[0].ForeignId = "ForeignId6";
+
+                  GivenList(1, true, new HashSet<int> { 3 }, true);
+                  GivenCleanLevel("disabled");
+                  GivenExistingMovie("ForeignId6", new HashSet<int> { 3 });
+
+                  Subject.Execute(_commandAll);
+
+                  Mocker.GetMock<IMovieService>()
+                        .Verify(v => v.UpdateMovie(It.IsAny<List<Movie>>(), It.IsAny<bool>()), Times.Never());
+            }
+
+            [Test]
+            public void should_tag_the_studio_and_performers_of_an_existing_movie()
+            {
+                  _importListFetch.Movies.ForEach(m => m.ListId = 1);
+                  _importListFetch.Movies[0].ForeignId = "ForeignId6";
+
+                  GivenList(1, true, new HashSet<int> { 3 }, true);
+                  GivenCleanLevel("disabled");
+                  GivenExistingMovie("ForeignId6", new HashSet<int>(), "Studio1", "Performer1", "Performer2");
+
+                  Mocker.GetMock<IStudioService>()
+                        .Setup(v => v.FindByForeignIds(It.IsAny<List<string>>()))
+                        .Returns(new List<Studio> { new Studio { Id = 1, ForeignId = "Studio1", Tags = new HashSet<int>() } });
+
+                  Mocker.GetMock<IPerformerService>()
+                        .Setup(v => v.FindByForeignIds(It.IsAny<List<string>>()))
+                        .Returns(new List<Performer>
+                        {
+                              new Performer { Id = 1, ForeignId = "Performer1", Tags = new HashSet<int>() },
+                              new Performer { Id = 2, ForeignId = "Performer2", Tags = new HashSet<int> { 3 } }
+                        });
+
+                  Subject.Execute(_commandAll);
+
+                  Mocker.GetMock<IStudioService>()
+                        .Verify(v => v.Update(It.Is<List<Studio>>(s => s.Count == 1 && s[0].Tags.SetEquals(new[] { 3 }))), Times.Once());
+
+                  // Performer2 already carried the tag, so only Performer1 is written back.
+                  Mocker.GetMock<IPerformerService>()
+                        .Verify(v => v.Update(It.Is<List<Performer>>(p => p.Count == 1 && p[0].ForeignId == "Performer1")), Times.Once());
+            }
+
+            [Test]
+            public void should_not_look_up_studios_or_performers_when_the_existing_movie_has_none()
+            {
+                  _importListFetch.Movies.ForEach(m => m.ListId = 1);
+                  _importListFetch.Movies[0].ForeignId = "ForeignId6";
+
+                  GivenList(1, true, new HashSet<int> { 3 }, true);
+                  GivenCleanLevel("disabled");
+                  GivenExistingMovie("ForeignId6", new HashSet<int>());
+
+                  Subject.Execute(_commandAll);
+
+                  Mocker.GetMock<IStudioService>()
+                        .Verify(v => v.FindByForeignIds(It.IsAny<List<string>>()), Times.Never());
+
+                  Mocker.GetMock<IPerformerService>()
+                        .Verify(v => v.FindByForeignIds(It.IsAny<List<string>>()), Times.Never());
+            }
+
+            [Test]
+            public void should_merge_tags_from_every_list_that_matches_an_existing_movie()
+            {
+                  _list2Movies.ForEach(m => m.ListId = 2);
+                  _importListFetch.Movies.ForEach(m => m.ListId = 1);
+                  _importListFetch.Movies[0].ForeignId = "ForeignId6";
+                  _importListFetch.Movies.AddRange(_list2Movies);
+
+                  // ForeignId6 is on both lists: list 2 already carries it out of the box.
+                  GivenList(1, true, new HashSet<int> { 3 }, true);
+                  GivenList(2, true, new HashSet<int> { 4 }, true);
+                  GivenCleanLevel("disabled");
+                  GivenExistingMovie("ForeignId6", new HashSet<int>());
+
+                  Subject.Execute(_commandAll);
+
+                  Mocker.GetMock<IMovieService>()
+                        .Verify(v => v.UpdateMovie(It.Is<List<Movie>>(m => m.Count == 1 && m[0].Tags.SetEquals(new[] { 3, 4 })), true), Times.Once());
+            }
+
+            [Test]
+            public void should_merge_tags_from_every_list_that_wants_a_new_movie()
+            {
+                  _list2Movies.ForEach(m => m.ListId = 2);
+                  _list2Movies[0].ForeignId = "ForeignId4";
+                  _importListFetch.Movies.ForEach(m => m.ListId = 1);
+                  _importListFetch.Movies.AddRange(_list2Movies);
+
+                  GivenList(1, true, new HashSet<int> { 3 });
+                  GivenList(2, true, new HashSet<int> { 4 });
+                  GivenCleanLevel("disabled");
+
+                  Subject.Execute(_commandAll);
+
+                  Mocker.GetMock<IAddMovieService>()
+                        .Verify(v => v.AddMovies(It.Is<List<Movie>>(s => s.Single(m => m.ForeignId == "ForeignId4").Tags.SetEquals(new[] { 3, 4 })), true), Times.Once());
+            }
+
+            [Test]
+            public void should_not_mutate_the_import_list_definition_tags()
+            {
+                  var listTags = new HashSet<int> { 3 };
+
+                  _list2Movies.ForEach(m => m.ListId = 2);
+                  _list2Movies[0].ForeignId = "ForeignId4";
+                  _importListFetch.Movies.ForEach(m => m.ListId = 1);
+                  _importListFetch.Movies.AddRange(_list2Movies);
+
+                  GivenList(1, true, listTags);
+                  GivenList(2, true, new HashSet<int> { 4 });
+                  GivenCleanLevel("disabled");
+
+                  Subject.Execute(_commandAll);
+
+                  listTags.Should().BeEquivalentTo(new[] { 3 });
             }
       }
 }
