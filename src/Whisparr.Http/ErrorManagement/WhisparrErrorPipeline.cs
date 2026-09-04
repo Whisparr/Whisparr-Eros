f58cb78b62ae050a2be3007e6d1bbd798c8d16ff
plz12345 <132735020+plz12345@users.noreply.github.com>
2026-09-04 01:45:08 -0400

    Fixed: Strip control characters from exclusion log values

--- src/Whisparr.Http/ErrorManagement/WhisparrErrorPipeline.cs
+++ src/Whisparr.Http/ErrorManagement/WhisparrErrorPipeline.cs
@@ -5,6 +5,7 @@ using FluentValidation;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using NLog;
+using NzbDrone.Common.Extensions;
 using NzbDrone.Common.Serializer;
 using NzbDrone.Core.Datastore;
 using NzbDrone.Core.Exceptions;
@@ -58,7 +59,7 @@ namespace Whisparr.Http.ErrorManagement
             }
             else if (exception is ExcludedException excludedException)
             {
-                _logger.Info("Request refused by exclusion rules. {0} {1}: {2}", context.Request.Method, context.Request.Path, excludedException.Message);
+                _logger.Info("Request refused by exclusion rules. {0} {1}: {2}", context.Request.Method.ForLog(), context.Request.Path.ToString().ForLog(), excludedException.Message.ForLog());
 
                 errorModel.Description = null;
                 statusCode = HttpStatusCode.Conflict;
